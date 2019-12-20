f600ba4b157d3023407939f11a8e12507c636962
hassenelber <elberkennou.php@gmail.com>
2019-12-20 22:15:27 +0800

    Format Document

--- usersApp.js
+++ usersApp.js
@@ -1,22 +1,16 @@
 const express = require("express");
 const app = express();
-
-
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
-
-
 const path = require ('path');
 
-//Routes
 
+//Routes
 const usersRoutes = require("./api/usersRoutes");
 
 
-
-
+// DataBase
 mongoose.connect( process.env.database, { useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set('useCreateIndex', true)
 const db = mongoose.connection;
@@ -30,10 +24,9 @@ db.once('connected', () =>{
 })
 
 
-
-
 //Log
 app.use(morgan("dev"));
+
 
 // app.use('/uploads', express.static('uploads'));
 
@@ -46,7 +39,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 
 //Cors
-
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -61,15 +53,15 @@ app.use((req, res, next) => {
 });
 
 
-
-// Set Static Folder
+//Set Static Folder
 app.use(express.static(path.join(__dirname,'../client')));
 
-// Routes which should handle requests
 
+//Routes which should handle requests
 app.use('/users', usersRoutes);
 
 
+//Express App Error
 app.use((req, res, next) => {
   const error = new Error("Not found");
   error.status = 404;
@@ -84,5 +76,6 @@ app.use((error, req, res, next) => {
     }
   });
 });
+
 
 module.exports = app;
